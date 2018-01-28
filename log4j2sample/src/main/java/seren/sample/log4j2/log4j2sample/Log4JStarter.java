68d1e0678b8246b336cd1dd9efa2109f69f6a5a3
A-pZ <a-pz@h4.dion.ne.jp>
2018-01-28 22:01:33 +0900

    log4j start kit.

---
+++ log4j2sample/src/main/java/seren/sample/log4j2/log4j2sample/Log4JStarter.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+package seren.sample.log4j2.log4j2sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author A-pZ
+ *
+ */
+@Log4j2
+public class Log4JStarter {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Log4JStarter l = new Log4JStarter();
+		l.start();
+	}
+
+	public void start() {
+		List<String> arrays = Arrays.asList("1,3,6,10,15");
+		log.debug("logging sample -> {}", arrays);
+	}
+}
